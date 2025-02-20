b70e63d628d7da70529811d51d0ede8dccb55faa
Tim Feeley <tim@timfeeley.com>
2025-02-20 10:46:51 -0800

    Initial Commit

---
+++ gontime/gontimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gontimeApp.swift
+//  gontime
+//
+//  Created by Tim Feeley on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct gontimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
